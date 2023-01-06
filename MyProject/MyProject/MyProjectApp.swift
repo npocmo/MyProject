709e6addd42c9be6fc9055435926186ccbe90991
Nesterenko, Dimitri <Dimitri.Nesterenko@comdirect.de>
2023-01-06 21:15:37 +0100

    added contentview from swift package

--- MyProject/MyProject/MyProjectApp.swift
+++ MyProject/MyProject/MyProjectApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import StartPlayer
 
 @main
 struct MyProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StartPlayer.StartPlayerContentView()
         }
     }
 }
---
+++ MyProject/StartPlayer/Sources/StartPlayer/StartPlayerContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+public struct StartPlayerContentView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        Text("Hello, StartPlayerContentView!")
+            .padding()
+    }
+}
+
+public struct StartPlayerContentView_Previews: PreviewProvider {
+    public static var previews: some View {
+        StartPlayerContentView()
+    }
+}
